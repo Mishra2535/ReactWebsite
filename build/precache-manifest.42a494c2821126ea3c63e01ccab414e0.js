b139ab69e32c74cab6c829450145ed47c75b33dd
Mishra2535 <70588547+Mishra2535@users.noreply.github.com>
2020-09-24 20:34:48 +0530

    Add files via upload

---
+++ build/precache-manifest.42a494c2821126ea3c63e01ccab414e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67938118181e49cbd0591f7e5e570a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f64d7d9b5f539c377a8b",
+    "url": "/static/css/main.bdb629d5.chunk.css"
+  },
+  {
+    "revision": "2f7834468de477610fda",
+    "url": "/static/js/2.afc82235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.afc82235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64d7d9b5f539c377a8b",
+    "url": "/static/js/main.f3f13f09.chunk.js"
+  },
+  {
+    "revision": "49e515e0c74cff36b645",
+    "url": "/static/js/runtime-main.de2667d6.js"
+  },
+  {
+    "revision": "6d84090300498a8f500ce3d4ed38961e",
+    "url": "/static/media/205.6d840903.jpg"
+  }
+]);
